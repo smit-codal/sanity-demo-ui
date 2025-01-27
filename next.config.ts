166246e2c9921e4a52f001517dc83fd1cbec4e53
sraghani <sraghani@codal.com>
2025-01-27 23:18:45 +0530

    create a basic blog page design

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "flowbite.s3.amazonaws.com",
+        pathname: "/**", // Corre
+      },
+      {
+        protocol: "https",
+        hostname: "flowbite.com",
+        pathname: "/**", // Corre
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,86 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
+        <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
+          <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+            <header className="mb-4 lg:mb-6 not-format">
+              <address className="flex items-center mb-6 not-italic">
+                <div className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                  <Image
+                    className="mr-4 w-16 h-16 rounded-full"
+                    src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
+                    alt="Jese Leos"
+                    width={100}
+                    height={100}
+                  />
+                  <div>
+                    <a
+                      href="#"
+                      rel="author"
+                      className="text-xl font-bold text-gray-900 dark:text-white"
+                    >
+                      Jese Leos
+                    </a>
+                    <p className="text-base text-gray-500 dark:text-gray-400">
+                      Graphic Designer, educator & CEO Flowbite
+                    </p>
+                    <p className="text-base text-gray-500 dark:text-gray-400">
+                      <time dateTime="2022-02-08" title="February 8th, 2022">
+                        Feb. 8, 2022
+                      </time>
+                    </p>
+                  </div>
+                </div>
+              </address>
+              <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
+                Best practices for successful prototypes
+              </h1>
+            </header>
+            <p className="my-5">
+              Flowbite is an open-source library of UI components built with the
+              utility-first classNamees from Tailwind CSS. It also includes
+              interactive elements such as dropdowns, modals, datepickers.
+            </p>
+            <p className="my-5">
+              Before going digital, you might benefit from scribbling down some
+              ideas in a sketchbook. This way, you can think things through
+              before committing to an actual design project.
+            </p>
+            <p className="my-5">
+              But then I found a{" "}
+              <a href="https://flowbite.com">
+                component library based on Tailwind CSS called Flowbite
+              </a>
+              . It comes with the most commonly used UI components, such as
+              buttons, navigation bars, cards, form elements, and more which are
+              conveniently built with the utility classNamees from Tailwind CSS.
+            </p>
+            <figure>
+              <Image
+                src="https://flowbite.s3.amazonaws.com/typography-plugin/typography-image-1.png"
+                alt=""
+                width={800}
+                height={800}
+                layout="responsive"
+              />
+              <div className="text-center mx-auto mt-5 mb-10">
+                Digital art by Anonymous
+              </div>
+            </figure>
+            <h2 className="font-semibold text-xl">
+              Getting started with Flowbite
+            </h2>
+            <p>
+              First of all you need to understand how Flowbite works. This
+              library is not another framework. Rather, it is a set of
+              components based on Tailwind CSS that you can just copy-paste from
+              the documentation.
+            </p>
+          </article>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
